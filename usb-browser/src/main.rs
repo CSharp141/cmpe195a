369476919719b4f7c3b876bc8318302977b1fd2e
Ryan Hedgecock <rhedgeco@gmail.com>
2023-11-30 16:15:32 -0800

    update site address to point to quiz

--- usb-browser/src/main.rs
+++ usb-browser/src/main.rs
@@ -2,20 +2,23 @@ use tao::{
     event::{DeviceEvent, ElementState, Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     keyboard::KeyCode,
-    window::WindowBuilder,
+    window::{Fullscreen, Theme, WindowBuilder},
 };
 
 use wry::webview::WebViewBuilder;
+
+static SITE_ADDR: &'static str = "https://usbproctor.com/exam";
 
 fn main() -> anyhow::Result<()> {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("USB Secure Proctor")
-        .with_fullscreen(Some(tao::window::Fullscreen::Borderless(None)))
+        .with_theme(Some(Theme::Dark))
+        .with_fullscreen(Some(Fullscreen::Borderless(None)))
         .build(&event_loop)?;
 
     let webview = WebViewBuilder::new(window)?
-        .with_url("https://usbproctor.com")?
+        .with_url(SITE_ADDR)?
         .with_devtools(false)
         .build()?;
 
@@ -30,7 +33,7 @@ fn main() -> anyhow::Result<()> {
             } => match event {
                 DeviceEvent::Key(key) => {
                     if key.state == ElementState::Pressed && key.physical_key == KeyCode::Space {
-                        webview.load_url("https://usbproctor.com");
+                        webview.load_url(SITE_ADDR);
                     }
                 }
                 _ => (),
